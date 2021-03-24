4f2505e218bbaf3e8274eeb273bfbfc2d2609edd
Jake Aringay <jakeraymund.aringay@gmail.com>
2021-03-24 22:12:02 +1100

    Add base layout EJS files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Call required packages for the app
 const express = require("express");
 const morgan = require("morgan");
+const ejsLayout = require("express-ejs-layouts");
 const path = require("path");
 const { port } = require("./config");
 
@@ -9,6 +10,14 @@ const app = express();
 
 // Monitors HTTP requests in the console
 app.use(morgan("dev"));
+
+// Set static path to serve static files
+app.set("/static", express.static(path.join(__dirname, "public")));
+
+// Set Layout
+app.set("view engine", "ejs");
+app.use(ejsLayout)
+app.set("layout", "./layouts/main-layout")
 
 // Load/Start application
 app.listen(port, () => {
